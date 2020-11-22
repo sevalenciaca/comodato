b9696efaa1a49885c63a2fd7a2f65437ec6e21de
sebasvalcar <sebasvc0921@gmail.com>
2020-11-21 20:32:47 -0500

    Inicializando proyecto comodato

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoComodato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
